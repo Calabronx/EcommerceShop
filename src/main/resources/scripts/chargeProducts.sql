d5cafcab9229475a0a81e06205ecdedce27e1f46
Calabronx <DarthVader2021>
2022-10-11 20:47:27 -0300

    Falto agregar el projecto de angular 9 frontend

---
+++ src/main/resources/scripts/chargeProducts.sql
@@ -0,0 +1,31 @@
+INSERT INTO products_tbl(id, product_name,product_price,product_quantity)
+VALUES (1, 'SMART TV SONY','50000',1);
+
+INSERT INTO products_tbl(id, product_name,product_price,product_quantity)
+VALUES (2, 'Remera VANS','1300',1);
+
+INSERT INTO products_tbl(id, product_name,product_price,product_quantity)
+VALUES (3, 'Skate ELEMENT','3550',1);
+
+INSERT INTO products_tbl(id, product_name,product_price,product_quantity)
+VALUES (4, 'LavaRopas','4500',1);
+
+INSERT INTO products_tbl(id, product_name,product_price,product_quantity)
+VALUES (5, 'Comic Spiderman','1200',1);
+
+INSERT INTO products_tbl(id, product_name,product_price,product_quantity)
+VALUES (6, 'Celular IPHONE','250000',1);
+
+INSERT INTO products_tbl(id, product_name,product_price,product_quantity)
+VALUES (7, 'Chevrolet Onix','890000',1);
+
+INSERT INTO products_tbl(id, product_name,product_price,product_quantity)
+VALUES (8, 'Parrilla','18000',1);
+
+INSERT INTO products_tbl(id, product_name,product_price,product_quantity)
+VALUES (9, 'Guitarra','16000',1);
+
+INSERT INTO products_tbl(id, product_name,product_price,product_quantity)
+VALUES (10, 'Bateria','20000',1);
+
+COMMIT;
